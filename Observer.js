9fbf59db3ac2ebdc0f39032eb20128ece75dc06f
fantasy525 <1056726491@qq.com>
2018-04-01 23:37:54 +0800

    add

--- Observer.js
+++ Observer.js
@@ -1,3 +1,56 @@
-export default function Observer(){
-  
+import Dep from './Dep'
+class Observer {
+  constructor(value) {
+    this.value = value // data数据
+    this.walk(this.value)
+  }
+  walk(obj) { //遍历data数据转换成setter和getter
+    const keys = Object.keys(obj)
+    for (let i = 0; i < keys.length; i++) {
+      defineReactive(obj, keys[i])
+    }
+  }
+}
+/**
+ * 创建一个Observer实例，返回新的 实例
+ * @param {any} value 
+ */
+export function observe(value) {
+  console.log('执行observe',value)
+  let ob
+  if (typeof value === 'object' && !Array.isArray(value)) {
+    ob = new Observer(value)
+  }
+  return ob
+}
+
+export function defineReactive(obj, key, val) {
+  let dep=new Dep()
+  val = obj[key]
+  let childOb = observe(val) //  递归属性的值 
+  console.log(key,val,childOb)
+ 
+  Object.defineProperty(obj, key, {
+    enumerable: true,
+    configurable: true,
+    get: function () {
+    
+      if ({}.toString.call(val) === '[object Function]') {
+        return val.call(data)
+      } else {
+        console.log('get')
+        return val
+      }
+    },
+    set: function (newVal) {
+      console.log('set')
+      if (val === newVal) {
+        return
+      }
+      
+      val = newVal
+      childOb = observe(val)//当重新设置值时给新值设置set和get
+      dep.notify()
+    }
+  })
 }
--- index.js
+++ index.js
@@ -1,79 +1,47 @@
-import Observer from './Observer.js'
-let V=function(){
-  function V(options){
-   this.options=options;
-   this._initData()
+import {observe,defineReactive} from './Observer.js'
+class V {
+  constructor(options) {
+    this.options = options;
+    this._initData()
   }
-  V.prototype={
-    _initData:function(){
-      var data=this._data=this.options.data
-      var keys=Object.keys(data)
-      var i,key;
-      i=keys.length;
-      while(i--){
-        key=keys[i]
+  _initData() {
+      var data = this._data = this.options.data
+      var keys = Object.keys(data)
+      var i, key;
+      i = keys.length;
+      while (i--) {
+        key = keys[i]
         this._proxy(key)
       }
-    },
+      observe(data)
+      
+    }
     /**
      * 代理data到vm实例上
      * @param {string} key data的属性值
      */
-    _proxy:function(key){
-      var self=this;
-      Object.defineProperty(self,key,{
-        configurable: true,
-        enumerable: true,
-        get: function proxyGetter () {
-          return self._data[key]
-        },
-        set: function proxySetter (val) {
-          self._data[key] = val
-        }
-      })
-    }
+  _proxy(key) {
+    var self = this;
+    Object.defineProperty(self, key, {
+      configurable: true,
+      enumerable: true,
+      get: function proxyGetter() {
+        return self._data[key]
+      },
+      set: function proxySetter(val) {
+        self._data[key] = val
+      }
+    })
   }
-  return V
-}()
-// function defineReactive(vm,prop,value){
- 
-//   Object.defineProperty(vm,prop,{
-//     get:function(){
-//       console.log('get')
-//       if({}.toString.call(value)==='[object Function]'){
-//         return value.call(data) 
-//       }
-//      else{
-//        return value
-//      }
-//     },
-//     set:function(newVal){
-//       console.log('set')
-//         if(value===newVal){
-//           return
-//         }
-//        value=newVal
-//     }
-//   })
-// }
-// function initData(){
-//   var data=vm.data
-// }
-
-// var data=vm.data
-// for(var k in data){
-//   if(data.hasOwnProperty(k)){
-//     defineReactive(vm,k,data[k])
-//   }
-// }
-var vm=new V({
+}
+const vm=new V({
   data:{
-    name:'苹果'
-  },
-  computed:{
-    food:function(){
-      return this.name
+    name:'zxf',
+    person:{
+      name:'name',
+      age:34
     }
   }
 })
+console.log(vm)
 
